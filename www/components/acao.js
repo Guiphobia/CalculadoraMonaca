f2a8561e7da0a9173222ec7e3d4a7ff8d13126f5
Guiphobia <dooguk3@gmail.com>
2024-08-24 09:14:11 +0900

    Project's Initial Commit

---
+++ www/components/acao.js
@@ -0,0 +1,9 @@
+$(document).on("click", "#Somar", function(){
+    var valor1 = $("#valor1").val();
+    var valor2 = $("#valor2").val();
+
+    var resultado = parseFloat(valor1) + parseFloat(valor2);
+
+    $("#resultado").val(resultado);
+} ) ;
+
